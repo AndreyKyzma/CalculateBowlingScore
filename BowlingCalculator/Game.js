3242868816b97b09cc77de6b71c1d0bfd6df63bc
AndreyKyzma <angeljugler@rambler.ru>
2015-10-27 03:11:53 +0200

    testfunct

---
+++ BowlingCalculator/Game.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Кузя on 27.10.2015.
+ */
+function roll(){
+
+}
+
+function score(){
+
+}
---
+++ BowlingCalculator/calculatorMain.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Кузя on 27.10.2015.
+ */
+var ScoreBowling = function () {
+    this.throwBall = [];
+    this.currentThrowBall = 0;
+};
+
+ScoreBowling.prototype.roll = function (pins) {
+    this.throwBall[this.currentThrowBall++] = pins;
+};
+
+
+ScoreBowling.prototype.score = function () {
+    var score = 0;
+    var frameIndex = 0;
+    var self = this;
+
+    function sumOfBallsInGameFrame() {
+        return self.throwBall[frameIndex] + self.currentThrowBall[frameIndex + 1];
+    }
+
+    function strikeBonus() {
+        return self.throwBall[frameIndex + 1] + self.throwBall[frameIndex + 2];
+    }
+
+    function spareBonus() {
+        return self.throwBall[frameIndex + 2];
+    }
+
+    function isStrike() {
+        return self.throwBall[frameIndex] === 10;
+    }
+
+    function isSpare() {
+        return self.throwBall[frameIndex] + self.throwBall[frameIndex + 1] === 10;
+    }
+
+    for (var frame = 0; frame < 10; frame++) {
+        if (isStrike()) {
+            score += 10 + strikeBonus();
+            frameIndex++;
+        }
+        else if (isSpare()) {
+            score += 10 + spareBonus();
+            frameIndex += 2;
+        }
+        else {
+            score += sumOfBallsInGameFrame();
+            frameIndex += 2;
+        }
+    }
+    return score;
+};
